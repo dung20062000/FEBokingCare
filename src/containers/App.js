905dc2bc2ada5e31303768523b38ba53070d4a70
Tien Dung <dungbuivm@gmail.com>
2023-05-06 16:26:29 +0700

    Header Admin Hiển Thị Đa Ngôn Ngữ

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,9 +13,7 @@ import {
 import { path } from "../utils";
 
 import Home from "../routes/Home";
-// import Login from '../routes/Login';
 import Login from "./Auth/Login";
-import Header from "./Header/Header";
 import System from "../routes/System";
 
 import { CustomToastCloseButton } from "../components/CustomToast";
@@ -49,7 +47,6 @@ class App extends Component {
                 <Router history={history}>
                     <div className="main-container">
                         <ConfirmModal />
-                        {this.props.isLoggedIn && <Header />}
 
                         <div className="content-container">
                             <CustomScrollbars style = {{height:'100vh', width: '100%'}}>
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -4,11 +4,15 @@ import { connect } from "react-redux";
 import * as actions from "../../store/actions";
 import Navigator from "../../components/Navigator";
 import { adminMenu } from "./menuApp";
+import {LANGUAGES} from "../../utils"
 import "./Header.scss";
 
 class Header extends Component {
+    handleChangeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language)
+    }
     render() {
-        const { processLogout } = this.props;
+        const { processLogout, language } = this.props;
 
         return (
             <div className="header-container">
@@ -16,11 +20,22 @@ class Header extends Component {
                 <div className="header-tabs-container">
                     <Navigator menus={adminMenu} />
                 </div>
+                <div className="languages">
+                    <span 
+                        className={language === LANGUAGES.VI ? 'languages-vi active' : 'languages-vi'} 
+                        onClick={() => this.handleChangeLanguage(LANGUAGES.VI)} 
+                    >VN</span>
+                    <span 
+                        className={language === LANGUAGES.EN ? 'languages-en active' : 'languages-en'} 
+                        onClick={() => this.handleChangeLanguage(LANGUAGES.EN)} >EN</span>
 
-                {/* nút logout */}
-                <div className="btn btn-logout" onClick={processLogout}>
-                    <i className="fas fa-sign-out-alt"></i>
+                    {/* nút logout */}
+                    <div className="btn btn-logout" onClick={processLogout} title="Logout">
+                        <i className="fas fa-sign-out-alt"></i>
+                    </div>
                 </div>
+
+
             </div>
         );
     }
@@ -29,12 +44,15 @@ class Header extends Component {
 const mapStateToProps = (state) => {
     return {
         isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
         processLogout: () => dispatch(actions.processLogout()),
+        //fire action của redux (có tên là changeLanguageApp)
+        changeLanguageAppRedux: (language) => dispatch(actions.changeLanguageApp(language))
     };
 };
 
